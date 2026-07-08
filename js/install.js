a3704c5093141eb0f995b2b699976529b58cff73
Kevin Sáenz <kevinsaenz503sv@gmail.com>
2026-07-07 20:27:56 -0600

    Update install.js

--- js/install.js
+++ js/install.js
@@ -1,1 +1,33 @@
+let deferredPrompt;
 
+const btn = document.getElementById("btnInstalar");
+
+window.addEventListener("beforeinstallprompt", (e) => {
+
+    e.preventDefault();
+
+    deferredPrompt = e;
+
+    btn.hidden = false;
+
+});
+
+btn.addEventListener("click", async () => {
+
+    btn.hidden = true;
+
+    deferredPrompt.prompt();
+
+    const { outcome } = await deferredPrompt.userChoice;
+
+    console.log("Resultado:", outcome);
+
+    deferredPrompt = null;
+
+});
+
+window.addEventListener("appinstalled", () => {
+
+    alert("Agenda Estudiantil instalada correctamente.");
+
+});
